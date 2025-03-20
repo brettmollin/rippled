8965e79b8eb797328f343f2c3eeec1987c3730f3
Ed Hennis <ed@ripple.com>
2025-03-19 20:02:57 -0400

    refactor: Check transaction flags in preflight0
    - Adds a flagMask parameter to preflight1 so that it's impossible to
      forget to check flags.
    - Also adds a short hash prefix to all Transactor log messages.

--- src/xrpld/app/tx/detail/VaultDelete.cpp
+++ src/xrpld/app/tx/detail/VaultDelete.cpp
@@ -35,11 +35,8 @@ VaultDelete::preflight(PreflightContext const& ctx)
     if (!ctx.rules.enabled(featureSingleAssetVault))
         return temDISABLED;
 
-    if (auto const ter = preflight1(ctx))
+    if (auto const ter = preflight1(ctx, tfUniversalMask))
         return ter;
-
-    if (ctx.tx.getFlags() & tfUniversalMask)
-        return temINVALID_FLAG;
 
     if (ctx.tx[sfVaultID] == beast::zero)
         return temMALFORMED;
